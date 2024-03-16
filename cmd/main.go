d405687fa129ec5963c9ca318c86800a846e3d53
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2024-03-16 11:57:23 +0100

    fix: removed dead code

--- cmd/main.go
+++ cmd/main.go
@@ -11,18 +11,5 @@ func init() {
 }
 
 func main() {
-
-	// client.InitErc20Balance("0x0E85ef63337299eCF06dEBa43a61ce8B5982B38e")
-
-	// client.InitBalance("0xeca881a5f23c62b75f76390Ee8aBFA441F681A0e", 7766279631452241920)
-
 	command.Run()
 }
-
-// func main() {
-// 	client := NewClient("https://rpc.tenderly.co/fork/835ecb4e-1f60-4329-adc2-b0c741193830")
-
-// 	client.InitErc20Balance("0x0E85ef63337299eCF06dEBa43a61ce8B5982B38e")
-// 	client.InitBalance("0x0E85ef63337299eCF06dEBa43a61ce8B5982B38e")
-// 	// fmt.Println(client.LatestBlock())
-// }
